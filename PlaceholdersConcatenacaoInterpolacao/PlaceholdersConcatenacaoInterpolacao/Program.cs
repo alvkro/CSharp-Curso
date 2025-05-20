832514cb97d5e2abbe14ec5faed2f2c7a146ad94
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-05-20 17:32:07 -0300

    Add files via upload

---
+++ PlaceholdersConcatenacaoInterpolacao/PlaceholdersConcatenacaoInterpolacao/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace PCI
+{
+    class PlaceConcaInter
+    {
+        static void Main(string[] args)
+        {
+            int idade = 22;
+            double saldo = 22.5751;
+            string name = "Fulano";
+
+            //Via Placeholder:
+            Console.WriteLine("{0} tem {1} anos e possui {2:F2} reais.", name, idade , saldo);
+
+            //Via Interpolação:
+            Console.WriteLine($"Seu nome é {name}, tem {idade} anos e possui um saldo de {saldo:F2}");
+
+            //Via concatenação de strings (arcaico):
+            Console.WriteLine(name + " tem " + idade + "anos de idade e possui um saldo de " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais.");
+
+            //Via F-String:
+            string msg = $"Seu nome é {name}, tem {idade} anos e possui um saldo de {saldo}";
+            Console.WriteLine(msg);//O F-String do C# usa o símbolo de $, semelhante a interpolação.
+        }
+    }
+}
---
+++ SaidaDados/SaidaDados/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization; // "Universaliza" o código (lembre-se de geografia)
+
+namespace Dados
+{
+    class SaidaDados
+    {
+        static void Main(string[] args)
+        {
+            char genero = 'F';
+            int idade = 23;
+            double saldo = 23.75234;
+            string nome = "Maria";
+
+            Console.Write("Bom dia");
+            Console.WriteLine("Boa tarde"); //WriteLine inclui uma quebra de linha
+            Console.WriteLine("Boa noite");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo.ToString("F2"));                               //Esse comando ToString() serve para especificar a precisão
+            Console.WriteLine(nome);                                               //das casas decimais e arredonda, semelhante ao :.2f do Python :)
+            Console.WriteLine(saldo.ToString("F2", CultureInfo.InvariantCulture)); //Essa função CultureInfo.InvariantCulture serve justamente para tirar a 
+        }                                                                          //vírgula e colocar o ponto, "deslocaliza" o programa e torna-o "universal" :P
+        
+    }
+}
+
