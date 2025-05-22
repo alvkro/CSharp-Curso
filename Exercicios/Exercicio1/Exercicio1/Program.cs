167f2bcf6ea57b08a7dd7eb69928859398acbc8b
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-05-22 20:44:08 -0300

    Add files via upload

---
+++ Exercicios/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio1
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            //VARIAVEIS
+            string produto1 = "Computador";
+            string produto2 = "Mesa";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.0;
+            double preco2 = 650;
+            double medida = 53.234567;
+
+            //OUTPUT
+            Console.WriteLine("PRODUTOS:");
+            Console.WriteLine($"{produto1} - R${preco1}");
+            Console.WriteLine($"{produto2} - R${preco2}");
+            Console.WriteLine($"");
+            Console.WriteLine($"Registro: {idade} anos de idade, codigo {codigo} e gênero {genero}");
+            Console.WriteLine($"Arredondado (três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal: " + medida.ToString("F3", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ Exercicios/Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Curso
+{
+    class Exercicio2
+    {
+        static void Main(string[] args)
+        {
+            //Nome Sobrenome
+            string[] vet_ns = Console.ReadLine().Split(' ');
+            string nome = vet_ns[0];
+            string sobrenome = vet_ns[1];
+
+            int quartos = int.Parse(Console.ReadLine());
+
+            double preco = double.Parse(Console.ReadLine());
+
+            //Idade altura
+            string[] vet_ia = Console.ReadLine().Split(' ');
+            int idade = int.Parse(vet_ia[0]);
+            double altura = double.Parse(vet_ia[1]);
+
+
+            Console.WriteLine($"{nome} {sobrenome}, {quartos}, {preco}, {sobrenome}, {idade} e {altura}");
+        }
+    }
+}
