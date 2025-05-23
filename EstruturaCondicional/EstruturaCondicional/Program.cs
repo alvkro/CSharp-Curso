15794e93f46636a62b4cd366ee2aa789761e59e8
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-05-23 10:22:27 -0300

    Add files via upload

---
+++ EstruturaCondicional/EstruturaCondicional/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Curso
+{
+    class EstruturaLogica
+    {
+        static void Main(string[] args)
+        {
+            // Existem 3 tipos de estruturas lógicas: simples, compostas e encadeadas.
+
+            int x = int.Parse(Console.ReadLine());
+
+            // Simples:
+
+            if (x > 5)
+            {
+                Console.WriteLine("Teste");
+            }
+
+            // Composta:
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("Par!");
+            }
+            else
+            {
+                Console.WriteLine("Impar!");
+            }
+
+            // Encadeada:
+            if (x == 12)
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if (x < 18)
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+            // Utilizando o else-if, o programa testará apenas as condições necessárias,
+            // ou seja, se a primeira funcionar, ela nem olha para as outras, otimizando o programa,
+            // e não será necessário repetir a condição de novo, como no primeiro else-if, não sendo
+            // necessário colocar x > 12 || x < 18, afinal, se foi para o else-if é porque a primera condição
+            // deu False.
+            }
+        }
+    }
---
+++ OperadoresLogicos/OperadoresLogicos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Curso
+{
+    class OperadoresLogicos
+    {
+        static void Main(string[] args)
+        {
+            //OPERADORES LÓGICOS EM C#:
+            //&&: E
+            //||: OU
+            //!: NÃO
+
+            bool c1 = 2 > 3 && 4 != 5; // false
+            bool c2 = !(2>3) && 4 != 5; // true
+            bool c3 = c2 || c1; // true
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+        }
+    }
+}
