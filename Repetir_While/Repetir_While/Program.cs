eeaec08df56dc382b71de822b66a16e9345d1ed0
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-05-29 22:45:39 -0300

    Add files via upload

---
+++ Repetir_While/Repetir_While/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Curso
+{
+    class While
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Digite um número: ");
+                double num = double.Parse(Console.ReadLine());
+
+                double sqrt = Math.Sqrt(num);
+
+                if (sqrt > 0)
+                {
+                    Console.WriteLine($"A raiz quadrada de {num} é {sqrt.ToString("F3")}");
+                }
+                else
+                {
+                    Console.WriteLine("Raiz negativa");
+                    break;
+                }
+            }
+        }
+    }
+}
