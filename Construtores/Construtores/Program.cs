3f77c98754f217b44db86f23245a6475cdeb56f3
Álvaro Barbosa Filho <alvaro.filho.111@ufrn.edu.br>
2025-07-01 11:14:43 -0300

    Add files via upload

---
+++ Construtores/Construtores/Program.cs
@@ -0,0 +1,42 @@
+﻿// Construtor: É uma operação especial da classe, que executa no momento da
+// instanciação do objeto, como ao criar um objeto a partir de uma classe. Serve para:
+// --> Iniciar valores dos atributos
+// --> Permitir ou obrigar que o objeto receba dados / dependências no momento de sua
+//     instanciação(injeção de dependência)
+// --> Construtor padrão: Produto p = new Produto();
+
+using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre os dados do produto:");
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+
+            Produto p = new Produto(nome, preco, quantidade);
+
+            Console.WriteLine();
+            Console.WriteLine("Dados do produto: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int qte = int.Parse(Console.ReadLine());
+            p.AdicionarProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+            Console.WriteLine();
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            qte = int.Parse(Console.ReadLine());
+            p.RemoverProdutos(qte);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + p);
+        }
+    }
+}
